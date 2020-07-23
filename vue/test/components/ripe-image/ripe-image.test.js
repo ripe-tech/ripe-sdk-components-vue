b196b1af0963f9e60fa3dd5fc35cbc91ed6258df
Margarida Silva <t-ms@platforme.com>
2020-07-23 10:44:37 +0100

    Test not valid model

--- vue/test/components/ripe-image/ripe-image.test.js
+++ vue/test/components/ripe-image/ripe-image.test.js
@@ -1,8 +1,15 @@
 const assert = require("assert");
 const base = require("../../base");
 
+class MockErrorRipeApi {
+    bindImage() {}
+    config() {
+        throw new Error("Error");
+    }
+}
+
 describe("RipeImage", () => {
-    it("should instantiate the component", async () => {
+    it("should instantiate the component", () => {
         const component = base.getComponent("RipeImage", {
             props: { brand: "dummy", model: "cube", version: 52, size: 1000 }
         });
@@ -10,12 +17,16 @@ describe("RipeImage", () => {
         assert.strictEqual(component.emitted("loading").length, 1);
     });
 
-    it("should return error when instantiating the component", async () => {
+    it("should return error when instantiating the component", () => {
         const component = base.getComponent("RipeImage", {
-            props: { brand: "dummy_not_valid", model: "cube_not_valid", version: 52, size: 1000 }
+            props: {
+                brand: "dummy_not_valid",
+                model: "cube_not_valid",
+                version: 52,
+                size: 1000,
+                ripeSdk: new MockErrorRipeApi()
+            }
         });
-
-        await new Promise(resolve => setTimeout(resolve, 500));
 
         assert.strictEqual(component.emitted("error").length, 1);
     });
