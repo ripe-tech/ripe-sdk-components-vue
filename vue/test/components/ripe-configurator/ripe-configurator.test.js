3672cae726c461bfad90ac1a5918d4db790072cb
Margarida Silva <t-ms@platforme.com>
2020-07-23 10:49:20 +0100

    Testing not valid model

--- vue/test/components/ripe-configurator/ripe-configurator.test.js
+++ vue/test/components/ripe-configurator/ripe-configurator.test.js
@@ -1,6 +1,19 @@
 const assert = require("assert");
 const base = require("../../base");
 
+class MockErrorRipeApi {
+    bindConfigurator() {
+        return {
+            bind() {},
+            resize() {}
+        };
+    }
+
+    config() {
+        throw new Error("Error");
+    }
+}
+
 describe("RipeConfigurator", () => {
     it("should instantiate the component", () => {
         const component = base.getComponent("RipeConfigurator", {
@@ -11,5 +24,19 @@ describe("RipeConfigurator", () => {
 
         assert.strictEqual(loader.exists(), true);
         assert.strictEqual(component.emitted("loading").length, 1);
+    });
+
+    it("should return error when instantiating the component", () => {
+        const component = base.getComponent("RipeConfigurator", {
+            props: {
+                brand: "dummy_not_valid",
+                model: "cube_not_valid",
+                version: 52,
+                size: 1000,
+                ripeSdk: new MockErrorRipeApi()
+            }
+        });
+
+        assert.strictEqual(component.emitted("error").length, 1);
     });
 });
