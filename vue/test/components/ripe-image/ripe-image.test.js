d62e94d180599aa405bd4e00e58f69318f36478d
Margarida Silva <t-ms@platforme.com>
2020-07-23 08:51:32 +0100

    Simple test

---
+++ vue/test/components/ripe-image/ripe-image.test.js
@@ -0,0 +1,12 @@
+const assert = require("assert");
+const base = require("../../base");
+
+describe("RipeImage", () => {
+    it("should instantiate the component", () => {
+        const component = base.getComponent("RipeImage", {
+            props: { brand: "dummy", model: "cube", version: 52, size: 1000 }
+        });
+
+        assert.strictEqual(component.emitted("loading").length, 1);
+    });
+});
