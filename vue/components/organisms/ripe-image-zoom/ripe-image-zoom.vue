3f72deaa14fc5807bafde3127548150e73974015
Margarida Silva <ms@platforme.com>
2020-10-19 15:56:05 +0100

    Improve comment

--- vue/components/organisms/ripe-image-zoom/ripe-image-zoom.vue
+++ vue/components/organisms/ripe-image-zoom/ripe-image-zoom.vue
@@ -42,8 +42,9 @@ export const RipeImageZoom = {
                 transform: `scale(${this.zoom / 100})`
             };
             if (this.pivot) {
-                // revert the translate after scaling the image so that the scaling
-                // appears centered on the pivot
+                // to center the image in the pivot even after scaling, the image is
+                // first centered in the pivot, scaled and then the translation is rectified
+                // by translating the reverse of the pivot
                 base.transform = `translate(${this.pivot.x}px, ${this.pivot.y}px) ${
                     base.transform
                 } translate(${-1 * this.pivot.x}px, ${-1 * this.pivot.y}px)`;
