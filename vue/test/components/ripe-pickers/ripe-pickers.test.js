0bce09240e5072b8e8d7c80f15e3ab3060008e53
NFSS10 <nuno_nfss96@hotmail.com>
2020-11-04 18:43:39 +0000

    Added test should sync selects options

--- vue/test/components/ripe-pickers/ripe-pickers.test.js
+++ vue/test/components/ripe-pickers/ripe-pickers.test.js
@@ -97,7 +97,6 @@ class MockRipeSdk {
 describe("RipePickers", () => {
     it("should instantiate the component", async () => {
         const ripeInstance = new MockRipeSdk();
-
         const component = await base.getComponent("RipePickers", { props: { ripe: ripeInstance } });
 
         assert.strictEqual(component.emitted("loading").length, 1);
@@ -106,5 +105,54 @@ describe("RipePickers", () => {
         assert.strictEqual(component.find(".select.parts").exists(), true);
         assert.strictEqual(component.find(".select.materials").exists(), true);
         assert.strictEqual(component.find(".select.colors").exists(), true);
+    });
+
+    it("should sync selects options", async () => {
+        const ripeInstance = new MockRipeSdk();
+        const component = await base.getComponent("RipePickers", { props: { ripe: ripeInstance } });
+
+        await component.vm.$forceUpdate();
+        assert.strictEqual(component.find(".select.parts").findAll("option").length, 4);
+        assert.strictEqual(component.find(".select.materials").findAll("option").length, 1);
+        assert.strictEqual(component.find(".select.colors").findAll("option").length, 1);
+
+        await component.setData({ selectedPart: "side" });
+        assert.strictEqual(component.find(".select.parts").findAll("option").length, 4);
+        assert.strictEqual(component.find(".select.materials").findAll("option").length, 4);
+        assert.strictEqual(component.find(".select.colors").findAll("option").length, 1);
+
+        await component.setData({ selectedPart: "patch" });
+        assert.strictEqual(component.find(".select.parts").findAll("option").length, 4);
+        assert.strictEqual(component.find(".select.materials").findAll("option").length, 2);
+        assert.strictEqual(component.find(".select.colors").findAll("option").length, 3);
+
+        await component.setData({ selectedPart: "top0_bottom" });
+        assert.strictEqual(component.find(".select.parts").findAll("option").length, 4);
+        assert.strictEqual(component.find(".select.materials").findAll("option").length, 4);
+        assert.strictEqual(component.find(".select.colors").findAll("option").length, 1);
+
+        await component.setData({ selectedPart: "side" });
+        await component.setData({ selectedMaterial: "crocodile_cbe" });
+        assert.strictEqual(component.find(".select.parts").findAll("option").length, 4);
+        assert.strictEqual(component.find(".select.materials").findAll("option").length, 4);
+        assert.strictEqual(component.find(".select.colors").findAll("option").length, 3);
+
+        await component.setData({ selectedPart: "side" });
+        await component.setData({ selectedMaterial: "leather_cbe" });
+        assert.strictEqual(component.find(".select.parts").findAll("option").length, 4);
+        assert.strictEqual(component.find(".select.materials").findAll("option").length, 4);
+        assert.strictEqual(component.find(".select.colors").findAll("option").length, 5);
+
+        await component.setData({ selectedPart: "top0_bottom" });
+        await component.setData({ selectedMaterial: "leather_cbe" });
+        assert.strictEqual(component.find(".select.parts").findAll("option").length, 4);
+        assert.strictEqual(component.find(".select.materials").findAll("option").length, 4);
+        assert.strictEqual(component.find(".select.colors").findAll("option").length, 5);
+
+        await component.setData({ selectedPart: "top0_bottom" });
+        await component.setData({ selectedMaterial: "suede_cbe" });
+        assert.strictEqual(component.find(".select.parts").findAll("option").length, 4);
+        assert.strictEqual(component.find(".select.materials").findAll("option").length, 4);
+        assert.strictEqual(component.find(".select.colors").findAll("option").length, 4);
     });
 });
