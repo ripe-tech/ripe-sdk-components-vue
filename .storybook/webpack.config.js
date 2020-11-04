0d6d470c3f5791daac89ab879c5f8974580876c6
Margarida Silva <ms@platforme.com>
2020-11-04 13:53:00 +0000

    Revert vercel.json and changed storybook webpack

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,15 +1,17 @@
 const path = require("path");
 
-module.exports = async ({ config, mode }) => {
-    config.module.rules.push({
-        test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"],
-        include: path.resolve(__dirname, "../")
-    });
-    config.module.rules.push({
-        test: /\.stories\.jsx?$/,
-        loaders: [require.resolve("@storybook/source-loader")],
-        enforce: "pre"
-    });
-    return config;
+module.exports = {
+    webpackFinal: (config) => {
+        config.module.rules.push({
+            test: /\.scss$/,
+            loaders: ["style-loader", "css-loader", "sass-loader"],
+            include: path.resolve(__dirname, "../")
+        });
+        config.module.rules.push({
+            test: /\.stories\.jsx?$/,
+            loaders: [require.resolve("@storybook/source-loader")],
+            enforce: "pre"
+        });
+        return config;
+    }
 };
