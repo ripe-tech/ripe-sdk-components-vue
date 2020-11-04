d5ee2fcb9a5ae940178bdde4e179a60453e3e127
Margarida Silva <ms@platforme.com>
2020-11-04 16:48:46 +0000

    Revert storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,16 @@
 const path = require("path");
 
-module.exports = {
-    webpackFinal: (config) => {
-        config.module.rules.push({
-            test: /\.scss$/,
-            loaders: ["style-loader", "css-loader", "sass-loader"],
-            include: path.resolve(__dirname, "../")
-        });
-        config.module.rules.push({
-            test: /\.stories\.jsx?$/,
-            loaders: [require.resolve("@storybook/source-loader")],
-            enforce: "pre"
-        });
-        return config;
-    }
+module.exports = async ({ config, mode }) => {
+    config.module.rules.push({
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"],
+        include: path.resolve(__dirname, "../")
+    });
+    config.module.rules.push({
+        test: /\.stories\.jsx?$/,
+        loaders: [require.resolve("@storybook/source-loader")],
+        enforce: "pre"
+    });
+    console.log(config, mode);
+    return config;
 };
