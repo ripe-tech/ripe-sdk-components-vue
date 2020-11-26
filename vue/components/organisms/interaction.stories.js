cb1605f9cf053f3f8386575a6651acb48ab0a1f2
Margarida Silva <ms@platforme.com>
2020-11-26 15:20:11 +0000

    New story with several components

---
+++ vue/components/organisms/interaction.stories.js
@@ -0,0 +1,64 @@
+import { storiesOf } from "@storybook/vue";
+import { withKnobs, text, number } from "@storybook/addon-knobs";
+
+storiesOf("Organisms", module)
+    .addDecorator(withKnobs)
+    .add("Interaction", () => ({
+        props: {
+            brand: {
+                type: String,
+                default: text("Brand", "dummy")
+            },
+            model: {
+                type: String,
+                default: text("Model", "cube")
+            },
+            version: {
+                type: Number,
+                default: number("Version", 52)
+            },
+            initials: {
+                default: text("Initials", "")
+            },
+            engraving: {
+                default: text("Engraving", "")
+            },
+            currency : {
+                default: text("Currency", "EUR")
+            },
+            frame: {
+                type: String,
+                default: text("Frame", "side-0")
+            },
+        },
+        computed: {
+            structure() {
+                return {
+                    brand: this.brand,
+                    model: this.model,
+                    version: this.version,
+                    parts: null,
+                    currency: this.currency,
+                    initials: this.initials,
+                    engraving: this.engraving,
+                    initials_extra: {}
+                }
+            }
+        },
+        template: `
+            <div>
+                <ripe-configurator
+                    v-bind:structure="structure"
+                    v-bind:size="400"
+                    style="display: inline-block"
+                />
+                <ripe-image 
+                    v-bind:structure="structure" 
+                    v-bind:frame="frame"
+                    v-bind:size="400"
+                />
+                <ripe-price v-bind:structure="structure" />
+                <ripe-pickers v-bind:structure="structure" />
+            </div>
+        `
+    }));
